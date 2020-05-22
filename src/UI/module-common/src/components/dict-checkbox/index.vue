f25ae59f3144d48cedff9ba227f907eca51736b0
Oldli <wenju1991@163.com>
2020-05-22 11:31:31 +0800

    新增字典radio和checkbox组件

---
+++ src/UI/module-common/src/components/dict-checkbox/index.vue
@@ -0,0 +1,29 @@
+<script>
+import { mixins } from 'netmodular-ui'
+
+const api = $api.common.dict
+
+export default {
+  mixins: [mixins.checkbox],
+  data() {
+    return {
+      action: this.query
+    }
+  },
+  props: {
+    group: {
+      type: String,
+      required: true
+    },
+    code: {
+      type: String,
+      required: true
+    }
+  },
+  methods: {
+    query() {
+      return api.select({ group: this.group, code: this.code })
+    }
+  }
+}
+</script>
