67e61f14cd8006da545e56f8aed4629cc20f8e2f
李文举 <liwenju@ftrans.cn>
2025-08-01 14:38:06 +0800

    升级到.net9

--- src/WebHost/Program.cs
+++ src/WebHost/Program.cs
@@ -6,7 +6,7 @@ namespace NetModular.Module.Common.WebHost
     {
         public static void Main(string[] args)
         {
-            new HostBuilder().Run<Startup>(args);
+            new HostBuilder().RunAsync(args);
         }
     }
 }
